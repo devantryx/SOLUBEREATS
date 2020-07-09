1e01785167f4451ec6faa13333078d7d399392c8
Gianmarcos Ventura <devantryx@gmail.com>
2020-07-09 13:32:42 -0500

    GVY :: correcion al cargar el proyecto

--- WebUberEats/Program.cs
+++ WebUberEats/Program.cs
@@ -16,36 +16,36 @@ namespace WebUberEats
     {
         public static void Main(string[] args)
         {
-            const string accountSid = "AC53e1eb902344a22504489db9f99f5c18";
-            const string authToken = "6170b6f5caf58910fa75b2e231e53f06";
+            //const string accountSid = "AC53e1eb902344a22504489db9f99f5c18";
+            //const string authToken = "6170b6f5caf58910fa75b2e231e53f06";
 
-            TwilioClient.Init(accountSid, authToken);
+            //TwilioClient.Init(accountSid, authToken);
 
-            GenerarCodigoUberEatsAleatorio();
+            //GenerarCodigoUberEatsAleatorio();
 
             CreateHostBuilder(args).Build().Run();
         }
 
-        [JSInvokable]
-        public static void GenerarCodigoUberEatsAleatorio()
-        {
-            for (int i = 0; i <= 1; i++)
-            {
-                var guid = Guid.NewGuid();
-                var justNumbers = new String(guid.ToString().Where(Char.IsDigit).ToArray());
-                var seed = int.Parse(justNumbers.Substring(0, 4));
+        //[JSInvokable]
+        //public static void GenerarCodigoUberEatsAleatorio()
+        //{
+        //    for (int i = 0; i <= 1; i++)
+        //    {
+        //        var guid = Guid.NewGuid();
+        //        var justNumbers = new String(guid.ToString().Where(Char.IsDigit).ToArray());
+        //        var seed = int.Parse(justNumbers.Substring(0, 4));
 
-                var random = new Random(seed);
+        //        var random = new Random(seed);
                
 
-                var message = MessageResource.Create(
-                   body: ($"{seed}"),
-                   from: new Twilio.Types.PhoneNumber("+12513579706"),
-                   to: new Twilio.Types.PhoneNumber("+51920162111") //nro a remplazar
-                   );
-                 Console.WriteLine(message.Sid);               
-            }       
-        }
+        //        var message = MessageResource.Create(
+        //           body: ($"{seed}"),
+        //           from: new Twilio.Types.PhoneNumber("+12513579706"),
+        //           to: new Twilio.Types.PhoneNumber("+51920162111") //nro a remplazar
+        //           );
+        //         Console.WriteLine(message.Sid);               
+        //    }       
+        //}
 
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
